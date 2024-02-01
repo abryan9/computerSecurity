94c79aa0e17bbe9d97f4c532aba9a50513919694
Alex Bryan <abryan9@uwyo.edu>
2024-02-01 11:42:10 -0700

    Encoding and brute force completed.

--- HW2/HW2-caesarCipher-DECODE.py
+++ HW2/HW2-caesarCipher-DECODE.py
@@ -1,7 +1,41 @@
 #!/user/bin/env python3
 
-def decode(ciphertext):
-    print(ciphertext)
+import os
 
-decode()
+def decode(ciphertext):
+    filepath = (__file__).removesuffix('HW2-caesarCipher-DECODE.py')
+    file = open(filepath + 'decodeOutput.txt', 'w')
+    outputList = []
+
+    for offset in range(26):
+        plaintext = ''
+
+        for char in ciphertext.lower():
+            if char.isalpha():
+                char = chr(ord(char) + offset)
+                while ord(char) > ord('z'):
+                    char = chr(ord(char) - 26)
+            
+            plaintext += char
+
+        if (not file.closed):
+            file.write(plaintext + '\n\n')
+        outputList.append(plaintext)
+
+    return outputList
+
+
+def analyze(bruteForceList):
+    for string in bruteForceList:
+        pass
+
+
+outputList = decode(input('Please enter a ciphertext to decode: '))
+
+print(
+    '[+] Decoded cipehertexts uploaded to decodeOutput.txt\n' +
+    '    Performing Frequency Analysis...'
+    )
+
+analyze(outputList)
 
